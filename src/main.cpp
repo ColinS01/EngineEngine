6ff86c81504b61e5a805a74d1b2030228558e240
ColinS14 <colin@optimotive.io>
2025-10-15 00:08:10 -0400

    removing glew and devcontainer, using glad instead on host

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,46 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+const double WIDTH = 800;
+const double HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, WIDTH, HEIGHT);
+}
 
 int main() {
+
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW\n";
         return -1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "EngineEngine", nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window\n";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-
     glfwMakeContextCurrent(window);
-    glewInit();
 
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }  
 
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
         glfwSwapBuffers(window);
-        glfwPollEvents();
+        glfwPollEvents();    
     }
 
     glfwTerminate();
