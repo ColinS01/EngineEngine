32fc27e99858ff64bf85f2a91f1cf6350b909bfd
ColinS14 <colin@optimotive.io>
2025-10-16 09:36:07 -0400

    adding indicies and a square

---
+++ include/core/Square.hpp
@@ -0,0 +1,16 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+#include <core/VisualObject.hpp>
+
+namespace Core
+{
+    class Square : public Core::VisualObject
+    {
+    public:
+        Square();
+        ~Square(){};
+    };
+}
+
+#endif // SQUARE_H
